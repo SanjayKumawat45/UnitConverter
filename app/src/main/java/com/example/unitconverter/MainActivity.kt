0de64635c6089b1856058d415570ce684722f992
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-12 00:43:38 +0530

    commit

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -5,19 +5,29 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,22 +46,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(){
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
 
 
         Row {
-            val context = LocalContext.current
-
-            Button(onClick = {
-                Toast
-                    .makeText(context,
-                        "Thanks for clicking!",
-                        Toast.LENGTH_LONG).show()
-            }) {
-                Text("Click me")
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
             }
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+            }
+
+
+
+
+
+//            val context = LocalContext.current
+//
+////            Button(onClick = {
+////                Toast
+////                    .makeText(context,
+////                        "Thanks for clicking!",
+////                        Toast.LENGTH_LONG).show()
+////            }) {
+////                Text("Click me")
+////            }
         }
     }
 }
