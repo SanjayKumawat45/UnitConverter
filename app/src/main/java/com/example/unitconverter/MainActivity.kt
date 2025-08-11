19e9e996b9d7d2b0359f95f4ac56b9e610d2975d
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-12 00:18:31 +0530

    commit

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -35,6 +39,20 @@ fun UnitConverter(){
     Column {
         Text("Unit Converter")
         OutlinedTextField(value = "", onValueChange = {})
+
+
+        Row {
+            val context = LocalContext.current
+
+            Button(onClick = {
+                Toast
+                    .makeText(context,
+                        "Thanks for clicking!",
+                        Toast.LENGTH_LONG).show()
+            }) {
+                Text("Click me")
+            }
+        }
     }
 }
 
